4c8e062e7e2a5e4e130d77040063b187a68ee78b
Alintya <Alintya@users.noreply.github.com>
2020-01-13 18:44:14 +0100

    add models folder

---
+++ PoeSuite/PoeSuite/Models/GameSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeSuite.Models
+{
+    class GameSettings
+    {
+        bool Fullscreen;
+        bool BorderlessWindow;
+        int ResolutionWidth;
+        int ResolutionHeight;
+        char ChatKey;
+    }
+}
---
+++ PoeSuite/PoeSuite/Models/TradeRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeSuite.Models
+{
+    class TradeRequest
+    {
+        string PlayerName;
+        string ItemName;
+        string StashTabName;
+        System.Numerics.Vector<int> ItemPosition;
+        int Price;
+        string CurrencyName;
+
+    }
+}
