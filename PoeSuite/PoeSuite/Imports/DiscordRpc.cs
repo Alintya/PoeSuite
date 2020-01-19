ff4c6594abb9523a083dbf37e4d6bb694effa51e
Alintya <Alintya@users.noreply.github.com>
2020-01-19 14:59:10 +0100

    refactor discordRpc

---
+++ PoeSuite/PoeSuite/Imports/DiscordRpc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeSuite.Imports
+{
+    internal class DiscordRpc
+    {
+        [DllImport("discord-rpc.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        public static extern void Discord_Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister, string optionalSteamId);
+
+        [DllImport("discord-rpc.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Discord_UpdatePresence(ref Models.RichPresence presence);
+
+        [DllImport("discord-rpc.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Discord_RunCallbacks();
+
+        [DllImport("discord-rpc.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Discord_Shutdown();
+
+        public struct EventHandlers
+        {
+            public ReadyCallback ReadyCallback;
+            public DisconnectedCallback DisconnectedCallback;
+            public ErrorCallback ErrorCallback;
+        }
+
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ReadyCallback();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void DisconnectedCallback(int errorCode, string message);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ErrorCallback(int errorCode, string message);
+    }
+}
---
+++ PoeSuite/PoeSuite/Models/RichPresence.cs
@@ -0,0 +1,34 @@
+﻿using PoeSuite.Imports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeSuite.Models
+{
+    [Serializable]
+    public struct RichPresence
+    {
+        public string State;
+        public string Details;
+        public long StartTimestamp;
+        public long EndTimestamp;
+        public string LargeImageKey;
+        public string LargeImageText;
+        public string SmallImageKey;
+        public string SmallImageText;
+        public string PartyId;
+        public int PartySize;
+        public int PartyMax;
+        public string MatchSecret;
+        public string JoinSecret;
+        public string SpectateSecret;
+        public bool Instance;
+
+        public void Update()
+        {
+            DiscordRpc.Discord_UpdatePresence(ref this);
+        }
+    }
+}
