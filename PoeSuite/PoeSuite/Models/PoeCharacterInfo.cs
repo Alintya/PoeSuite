8b92e877662f3ec7be252ce79e31e611bba0884f
Alintya <Alintya@users.noreply.github.com>
2020-01-19 15:36:55 +0100

    update PoeApi

---
+++ PoeSuite/PoeSuite/Models/PoeCharacterInfo.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeSuite.Models
+{
+    class PoeCharacterInfo
+    {
+        [JsonProperty("name")]
+        public string Name;
+
+        [JsonProperty("league")]
+        public string League;
+
+        [JsonProperty("classId")]
+        public int ClassId;
+
+        [JsonProperty("ascendancyClass")]
+        public int AscendancyClass;
+
+        [JsonProperty("class")]
+        public string Class;
+
+        [JsonProperty("level")]
+        public int Level;
+
+        [JsonProperty("experience")]
+        public int Experience;
+
+        [JsonProperty("lastActive")]
+        public bool LastActive;
+    }
+}
--- PoeSuite/PoeSuite/Utility/PoeApi.cs
+++ PoeSuite/PoeSuite/Utility/PoeApi.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using PoeSuite.Models;
+using System;
+using System.Net;
 
 namespace PoeSuite.Utility
 {
-    class PoeApi
+    internal class PoeApi
     {
+        private static WebClient _webclient = new WebClient();
+
+        public static PoeCharacterInfo GetCharacterData()
+        {
+            PoeCharacterInfo characterInfo = new PoeCharacterInfo();
+
+            try
+            {
+                var rawData = _webclient.DownloadString(
+                            $"https://www.pathofexile.com/character-window/get-characters?accountName={Properties.Settings.Default.AccountName}");
+
+                if (rawData.Length == 0)
+                {
+                    Logger.Get.Error("Data retrived from PoE api was empty");
+                    return null;
+                }
+                    
+                characterInfo = Array.Find(JsonConvert.DeserializeObject<PoeCharacterInfo[]>(rawData), x => x.LastActive);
+            }
+            catch (Exception ex)
+            {
+                Logger.Get.Error($"Failed to retrieve data from PoE api: {ex}");
+                return null;
+            }
+
+            return characterInfo;
+        }
     }
 }
