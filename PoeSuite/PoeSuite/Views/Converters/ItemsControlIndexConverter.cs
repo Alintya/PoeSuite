a79bfc5d421ef0699d3ccd8feb158505994cbc99
Alintya <alintya.lol@gmail.com>
2020-01-28 23:29:00 +0100

    move converter to folder

--- PoeSuite/PoeSuite/Views/Converters/ItemsControlIndexConverter.cs
+++ PoeSuite/PoeSuite/Views/Converters/ItemsControlIndexConverter.cs
@@ -2,7 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Data;
 
-namespace PoeSuite.Views
+namespace PoeSuite.Views.Converters
 {
     public class ItemsControlIndexConverter : IMultiValueConverter
     {
